e1704697ea145616f6a7d72ddd96f63a3262efb6
Joven Pancho <joven.pancho@ffuf.de>
2019-12-10 13:17:23 +0800

    Add drag and drop functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,8 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
+  exports: [DragDropModule],
   providers: [],
   bootstrap: [AppComponent]
 })
